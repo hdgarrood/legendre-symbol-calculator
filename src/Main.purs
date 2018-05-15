2052e9703042379c08ecdf30a4f1b9f6976ecc54
Harry Garrood <harry@garrood.me>
2018-05-15 01:39:32 +0100

    Explanation and CSS

--- src/Main.purs
+++ src/Main.purs
@@ -1,12 +1,24 @@
 module Main where
 
 import Prelude
+import Data.Maybe (Maybe(..))
+import DOM.Node.ParentNode (QuerySelector(..))
 import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Class (liftEff)
+import Control.Monad.Eff.Console (error)
 import Halogen.Aff as HA
 import Halogen.VDom.Driver (runUI)
 import Component (component)
 
-main :: Eff (HA.HalogenEffects ()) Unit
+selector :: QuerySelector
+selector = QuerySelector "#app"
+
+main :: Eff _ Unit
 main = HA.runHalogenAff do
-  body <- HA.awaitBody
-  runUI component unit body
+  HA.awaitLoad
+  mel <- HA.selectElement selector
+  case mel of
+    Just el ->
+      void $ runUI component unit el
+    Nothing ->
+      liftEff (error "Unable to find #app element")
