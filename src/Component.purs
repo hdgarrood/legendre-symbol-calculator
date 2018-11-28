4506195cd17a5ab0f51096797f1b0e003e494bda
Harry Garrood <harry@garrood.me>
2018-11-28 02:39:20 +0000

    Better validation UI

--- src/Component.purs
+++ src/Component.purs
@@ -1,27 +1,39 @@
 module Component where
 
 import Prelude
-import Control.Monad.Error.Class (throwError)
+import Control.MonadZero (guard)
 import Data.ModularArithmetic.Primality (isPrime)
 import Data.String as String
 import Data.Array as Array
+import Data.Foldable (for_)
 import Data.List as List
-import Data.Maybe (Maybe(..))
-import Data.Either (either, note)
+import Data.Maybe (Maybe(..), maybe)
 import Data.Tuple (Tuple(..), fst, snd)
 import Data.Int as Int
+import Effect.Aff (Aff)
+import Effect.Class (liftEffect)
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
 import LegendreSymbols as LS
+import Web.HTML.HTMLInputElement as HTMLInputElement
+import Web.HTML.HTMLDocument as HTMLDocument
+import Web.HTML.Window as Window
+import Web.HTML (window)
+import Web.DOM.NonElementParentNode (getElementById)
 
 type State =
+  -- The input is a tuple of the numerator and the denominator. The denominator
+  -- should always be an odd prime.
   { input :: Tuple Int Int
+  -- Count the number of reduction steps we should display; this is just the
+  -- number of times the 'Next' button has been pressed.
   , reductions :: Int
+  -- These fields just track what is in the UI, which may or may not be valid
+  -- inputs.
   , numerator :: String
   , denominator :: String
-  , validationError :: Maybe String
   }
 
 stateFromInput :: Tuple Int Int -> State
@@ -30,26 +42,25 @@ stateFromInput input =
   , reductions: 0
   , numerator: show (fst input)
   , denominator: show (snd input)
-  , validationError: Nothing
   }
 
-updateInput :: State -> State
-updateInput state =
+isOddPrime :: Int -> Boolean
+isOddPrime p = isPrime p && p > 2
+
+-- | Attempt to update the state based on the contents of the two input fields.
+-- | If the contents of the input fields do not constitute a valid input,
+-- | return the state unmodified.
+updateStateFromInputs :: State -> State
+updateStateFromInputs state =
   let
     newInput = do
-      a <- note "Numerator must be an integer"
-             (Int.fromString state.numerator)
-      p <- note "Denominator must be an integer"
-             (Int.fromString state.denominator)
-
-      when (not (isPrime p) || p == 2)
-        (throwError "Denominator must be an odd prime")
+      a <- Int.fromString state.numerator
+      p <- Int.fromString state.denominator
+      guard (isOddPrime p)
 
       pure (Tuple a p)
   in
-    either (\err -> state { validationError = Just err })
-           stateFromInput
-           newInput
+    maybe state stateFromInput newInput
 
 data Query a
   = Advance a
@@ -57,7 +68,7 @@ data Query a
   | UpdateNumerator String a
   | UpdateDenominator String a
 
-component :: forall m. H.Component HH.HTML Query Unit Void m
+component :: H.Component HH.HTML Query Unit Void Aff
 component =
   H.component
     { initialState: const initialState
@@ -86,28 +97,30 @@ component =
         # (_ == Just true)
 
       inputForm =
-        HH.div [ HP.class_ (HH.ClassName "input-form") ] $
+        HH.form [ HP.attr (H.AttrName "onsubmit") "return false" ]
           [ HH.span_ [ HH.text "Is" ]
           , HH.input
-              [ HP.type_ HP.InputText
+              [ HP.id_ numeratorId
+              , HP.type_ HP.InputText
               , HP.title "Numerator"
               , HP.value (show (fst state.input))
               , HE.onValueChange (HE.input UpdateNumerator)
               ]
           , HH.span_ [ HH.text "a quadratic residue modulo" ]
           , HH.input
-              [ HP.type_ HP.InputText
+              [ HP.id_ denominatorId
+              , HP.type_ HP.InputText
               , HP.title "Denominator"
               , HP.value (show (snd state.input))
               , HE.onValueChange (HE.input UpdateDenominator)
               ]
           , HH.span_ [ HH.text "?" ]
-          , HH.button
-              [ HP.title "Let's find out!"
+          , HH.input
+              [ HP.type_ HP.InputSubmit
+              , HP.value "Let's find out!"
               , HE.onClick (HE.input_ ReplaceInput)
               ]
-              [ HH.text "Let's find out!" ]
-          ] <> Array.fromFoldable (map renderValidationError state.validationError)
+          ]
 
       renderValidationError msg =
         HH.p_ [ HH.text msg ]
@@ -132,17 +145,53 @@ component =
     in
       HH.div_ [ inputForm, displayPanel, nextButton ]
 
-  eval :: Query ~> H.ComponentDSL State Query Void m
+  eval :: Query ~> H.ComponentDSL State Query Void Aff
   eval = case _ of
     Advance next -> do
       H.modify_ (\s -> s { reductions = s.reductions + 1 })
       pure next
     ReplaceInput next -> do
-      H.modify_ updateInput
+      H.modify_ updateStateFromInputs
       pure next
     UpdateNumerator new next -> do
+      H.liftAff (validateNumerator new)
       H.modify_ (\s -> s { numerator = new })
       pure next
     UpdateDenominator new next -> do
+      H.liftAff (validateDenominator new)
       H.modify_ (\s -> s { denominator = new })
       pure next
+
+  numeratorId :: String
+  numeratorId = "numerator"
+
+  denominatorId :: String
+  denominatorId = "denominator"
+
+  -- Given an input element id and a validation function, validate that input
+  -- element and update its custom validity status. The validation function
+  -- should return an empty string on success, or an error message on failure.
+  validateInputWith :: String -> (String -> String) -> String -> Aff Unit
+  validateInputWith elId validate value = liftEffect do
+    doc <- HTMLDocument.toNonElementParentNode <$> (window >>= Window.document)
+    mel <- getElementById elId doc
+    for_ (mel >>= HTMLInputElement.fromElement) \el ->
+      HTMLInputElement.setCustomValidity (validate value) el
+
+  validateNumerator :: String -> Aff Unit
+  validateNumerator =
+    validateInputWith numeratorId \value ->
+      case Int.fromString value of
+        Just _ -> ""
+        Nothing -> "The numerator must be an integer."
+
+  validateDenominator :: String -> Aff Unit
+  validateDenominator =
+    validateInputWith denominatorId \value ->
+      case Int.fromString value of
+        Nothing ->
+          "The denominator must be an integer."
+        Just p ->
+          if isOddPrime p
+            then ""
+            else "The denominator must be an odd prime."
